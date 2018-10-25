d0eb9c17455b5e3ff6974b990ed8e28e367f1c74
32087Joan <43579159+32087Joan@users.noreply.github.com>
2018-10-25 18:38:28 +0200

    primera version

---
+++ validacion.js
@@ -0,0 +1,13 @@
+var error="";
+		function validar(){
+			if (document.formulario1.usuario.value==""){
+				error+="El campo Usuario esta vacio.";}
+
+			if (document.formulario1.password.value=="") {
+				 error+="El campo Password esta vacio.";
+				}
+
+				alert(error);
+				return false;		
+		}
+
