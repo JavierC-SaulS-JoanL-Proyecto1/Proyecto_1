13b433ef2c602dc9cb0d40b9749f74a99d64a4d2
Grindcrust <43579091+SpicyTacos23@users.noreply.github.com>
2018-10-30 19:08:48 +0100

    Delete getInfoRecursosController.php

--- getInfoRecursosController.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('queriesMysql.php');
-$link = mysqli_connect('localhost', 'root', '', 'proyecto1bd');
-$id_recurso = $_POST['recurso'];
-$infoRecurso = queriesMysql::getInfoRecurso($link, (int)$id_recurso);
-echo $infoRecurso;
