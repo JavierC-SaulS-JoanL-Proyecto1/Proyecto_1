544a2fef127576f1cd3c454f32471f19410afbd1
Grindcrust <43579091+SpicyTacos23@users.noreply.github.com>
2018-10-30 17:55:20 +0100

    Add files via upload

--- getInfoRecursosController.php
+++ getInfoRecursosController.php
@@ -4,4 +4,3 @@ $link = mysqli_connect('localhost', 'root', '', 'proyecto1bd');
 $id_recurso = $_POST['recurso'];
 $infoRecurso = queriesMysql::getInfoRecurso($link, (int)$id_recurso);
 echo $infoRecurso;
-//$('.infoRecurso').html('Prueba 1');
