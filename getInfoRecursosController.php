0d5154f83d2e114be9db633362ada3e22c05da24
Peacetime5 <43578894+Peacetime5@users.noreply.github.com>
2018-10-31 19:24:34 +0100

    Delete getInfoRecursosController.php

--- getInfoRecursosController.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('queriesMysql.php');
-$link = mysqli_connect('localhost', 'root', '', 'proyecto1bd');
-$id_recurso = $_POST['recurso'];
-$infoRecurso = queriesMysql::getInfoRecurso($link, (int)$id_recurso);
-echo utf8_encode($infoRecurso[0])."@".$infoRecurso[1];
